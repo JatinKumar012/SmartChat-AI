7c9f9871e3650117447d632e35f848b387a8669f
Jatin Kumar <rajputjatin2018@gmail.com>
2025-08-03 17:16:25 +0530

    Changes in UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { SiDependabot } from "react-icons/si";
+import { FaRegCircleUser } from "react-icons/fa";
+
 
 const Navbar = () => {
   return (
@@ -7,7 +9,10 @@ const Navbar = () => {
        <div className="nav flex items-center justify-between  h-[100px] px-[150px]">
           <div className="logo flex items-center gap-[10px]">
             <i className='text-[50px]'><SiDependabot /></i>
-            <h3 className='text-[25px]'>Smart<span>Chat AI</span></h3>
+            <h3 className='text-[25px] font-[700]'>SmartChat<span className='text-purple-500'> AI</span></h3>
+          </div>
+          <div className="user">
+            <i><FaRegCircleUser/></i>
           </div>
        </div>
     </div>
